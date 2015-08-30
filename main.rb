b011d856e870523d8eb2def19135c117f0ca30c1
koudaiii <cs006061@gmail.com>
2015-08-30 21:14:14 +0900

    sample database in sinatra

--- main.rb
+++ main.rb
@@ -1,4 +1,13 @@
 require 'sinatra'
+require 'sqlite3'
+
+db = SQLite3::Database.new "db/test.db"
+db.results_as_hash = true
+
+books = db.execute("SELECT title, author, price FROM books;")
+
+p books
+
 get '/' do
    "Hello"
 end
